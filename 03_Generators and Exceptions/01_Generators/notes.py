b2a7b505d8b3c0505d9b5fa45c000c6b75435170
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-23 08:00:34 -0500

    updates

--- 03_Generators and Exceptions/01_Generators/notes.py
+++ 03_Generators and Exceptions/01_Generators/notes.py
@@ -2,16 +2,20 @@ def get_number():
     for i in range(1, 4):
         yield i
 
-print(get_number())
+
+
+# print(get_number())
 
 generator = get_number()
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
+# print(next(generator))
+# print(next(generator))
+# print(next(generator))
+# print(next(generator))
 
-for x in get_number():
+for x in generator:
     print(x)
+    
+generator2 = get_number()
 
-numbers = list(get_number())
+numbers = list(generator2)
 print(numbers)
--- 03_Generators and Exceptions/02_Exceptions and Syntax Errors/notes.py
+++ 03_Generators and Exceptions/02_Exceptions and Syntax Errors/notes.py
@@ -1,25 +1,25 @@
-if True:
-print('hooray!')
+# if True:    
+# print('hooray!')
 
-value = int(input('Enter an integer: '))
-print('The inverse of', value, 'is', 1/value)
+# value = int(input('Enter an integer: '))
+# print('The inverse of', value, 'is', 1/value)
 
-try:
-    value = int(input('Enter an integer: '))
-    print('The inverse of', value, 'is', 1/value)
-except:
-    print('You did not provide a number, so I will not calculate the inverse')
+# try:
+#     value = int(input('Enter an integer: '))
+#     print('The inverse of', value, 'is', 1/value)
+# except:
+#     print('You did not provide a number, so I will not calculate the inverse')
 
-value = int(input('Enter an integer: '))
-print('The inverse of', value, 'is', 1/value)
+# value = int(input('Enter an integer: '))
+# print('The inverse of', value, 'is', 1/value)
 
-try:
-    value = int(input('Enter an integer: '))
-    print('The inverse of', value, 'is', 1/value)
-except ValueError:
-    print('You did not provide a number, so I will not calculate the inverse')
-except ZeroDivisionError:
-    print('You provided 0 and division by 0 is not possible, sorry')
+# try:
+#     value = int(input('Enter an integer: '))
+#     print('The inverse of', value, 'is', 1/value)
+# except ValueError:
+#     print('You did not provide a number, so I will not calculate the inverse')
+# except ZeroDivisionError:
+#     print('You provided 0 and division by 0 is not possible, sorry')
 
 try:
     value = int(input('Enter an integer: '))
@@ -30,4 +30,5 @@ except ZeroDivisionError:
     print('You provided 0 and division by 0 is not possible, sorry')
 except:
     print('Something strange happened here, sorry')
+    
 
