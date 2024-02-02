a459ab37907f74666a91ebfc5b1e736653025a63
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-02 06:41:58 -0500

    updates

---
+++ 05_More on Exceptions/02_Raising Exceptions/Notes.py
@@ -0,0 +1,33 @@
+def return_bigger(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise ValueError
+    if b > a:
+        return b
+    else:
+        return a
+
+
+print(return_bigger(5, 3))
+
+
+print(return_bigger(5, 'b'))
+
+
+def return_reverse(x):
+    try:
+        return 1/x
+    except:
+        print('Something went wrong')
+
+
+return_reverse(0)
+
+
+def return_reverse(x):
+    try:
+        return 1/x
+    except:
+        print('Something went wrong')
+        raise
+
+
