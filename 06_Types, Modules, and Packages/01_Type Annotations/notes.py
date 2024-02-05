1047a8d0257a3159a7f65d5f5e0850c09b419637
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-05 11:49:49 -0500

    updates

--- 06_Types, Modules, and Packages/01_Type Annotations/notes.py
+++ 06_Types, Modules, and Packages/01_Type Annotations/notes.py
@@ -1,3 +1,52 @@
+def min(a : int, b: int) -> int:
+    if(a < b):
+        return a
+    else:
+        return b
+def print_min(a: int, b: int) -> None:
+    if (a < b):
+        print(a)
+    else:
+        print(b)   
+
+num1: int = int(input("Enter a number: "))
+num2 : int = int(input("Enter another number: "))
+# print(min(num1, num2))
+
+result = print_min(num1, num2)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def min(a: int, b : int) -> int:
     if (a < b):
         return a
@@ -5,11 +54,7 @@ def min(a: int, b : int) -> int:
         return b
 
 
-def print_min(a: int, b: int) -> None:
-    if (a < b):
-        print(a)
-    else:
-        print(b)
+
 
 
 number1: int = int(input("Enter a number"))
