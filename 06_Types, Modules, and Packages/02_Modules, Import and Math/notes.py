6ff36033a08be778f3c18c6785fd64953dcaf755
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-07 06:51:06 -0500

    updates

--- 06_Types, Modules, and Packages/02_Modules, Import and Math/notes.py
+++ 06_Types, Modules, and Packages/02_Modules, Import and Math/notes.py
@@ -1,87 +1,75 @@
-# # from sys import *
-# # from sys import exit
-# # import sys
-
-# # import sys
-# # import math
-# # import sys
-# # import math
-# # import sys
-
-# # for name in dir(sys):
-# #     print(name, end='\t')
-# # sys.exit()
-
-# # sys.exit324()
+# import sys
+# # # from sys import *
+# # from sys import exit, stdin
 
 
-# # def exit():
-# #     print('I wanna exit')
+# for name in dir(sys):
+#     print(name, end='\n')
+# sys.exit()
 
 
-# # exit()
-# # sys.exit()
-
-# # exit()
-
-
-# # def exit():
-# #     print('I wanna exit')
-
-
-# # exit()
-# # from sys import *
 # from sys import exit as bye_bye
-# import sys  # you need to add sys.
-# from sys import *
-# import sys as s
 
-# s.exit()
 
 # bye_bye()
 
 import math
 
-for name in dir(math):
-    print(name, end='\t')
+# for name in dir(math):
+#     print(name, end='\n')
     
 # ceil(), floor(), trunc()
-print(math.ceil(3.6))
-print(math.floor(3.6))
-print(math.trunc(3.6))
+# print(math.ceil(3.6))
+# print(math.floor(3.6))
+# print(math.trunc(3.6))
 
-# ceil()
-print(math.ceil(3.1))
-print(math.ceil(3.505))
-print(math.ceil(3.945))
-print(math.ceil(3.0))
-print(math.ceil(-5.4))
+# # ceil()
+# print(math.ceil(3.1))
+# print(math.ceil(3.505))
+# print(math.ceil(3.945))
+# print(math.ceil(3.0))
+# print(math.ceil(-5.4))
 
-# floor()
-print(math.floor(3.1))
-print(math.floor(3.505))
-print(math.floor(3.945))
-print(math.floor(3.0))
-print(math.floor(-5.4))
+# # floor()
+# print(math.floor(3.1))
+# print(math.floor(3.505))
+# print(math.floor(3.945))
+# print(math.floor(3.0))
+# print(math.floor(-5.4))
 
-# trunc()
-print(math.trunc(3.1))
-print(math.trunc(3.505))
-print(math.trunc(3.945))
-print(math.trunc(3.0))
-print(math.trunc(-5.4))
+# # # trunc()
+# print(math.trunc(3.1))
+# print(math.trunc(3.505))
+# print(math.trunc(3.945))
+# print(math.trunc(3.0))
+# print(math.trunc(-5.4))
 
 # factorial()
 # 3! = 3 * 2 * 1 = 6
-# 4! = 4 * 3 * 2 * 1 = 24
-print(math.factorial(3))
-print(math.factorial(4))
+# 4! = 4 * 3! = 4 * 3 * 2 * 1 = 24
+# print(math.factorial(3))
+# print(math.factorial(4))
+# print(math.factorial(23))
 
-# sqrt()
-print(math.sqrt(16))
-print(math.sqrt(100))
+# # sqrt()
+# print(math.sqrt(16))
+# print(math.sqrt(100))
 
-# hypot()
-print(math.hypot(3, 4))
+# print(0.1 + 0.2)
+# print( 1/ 7)
+
+# #Other math
+# ang_rad = math.radians(30)
+# print(math.sin(ang_rad))
+
+print(math.e)
+print(math.pi)
+
+a = 3
+b = 4
+c = math.sqrt(math.pow(3, 2) + math.pow(4, 2))
+c = math.sqrt(3**2 + 4**2)
+c = math.hypot(3, 4)
+
 
 
