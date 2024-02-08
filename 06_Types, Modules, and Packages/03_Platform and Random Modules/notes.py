b247ef4758a877f8ea505dae49fe40346aa5fe39
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-08 06:46:33 -0500

    updates

--- 06_Types, Modules, and Packages/03_Platform and Random Modules/notes.py
+++ 06_Types, Modules, and Packages/03_Platform and Random Modules/notes.py
@@ -1,54 +1,58 @@
-import random
 import platform
+import random
+
+# print(platform.platform())
 
 
-platform.platform()
+# print(platform.platform(True, True))
+# print(platform.machine())
 
 
-platform.platform(True, True)
-platform.machine()
+# print(platform.processor())
 
 
-platform.processor()
+# print(platform.system())
 
 
-platform.system()
+# print(platform.python_implementation())
 
 
-platform.python_implementation()
+# print(platform.python_version_tuple())
 
 
-platform.python_version_tuple()
+# print(random.random())
+# print(random.random())
+# print(random.random())
 
-
-print(random.random())
-print(random.random())
-print(random.random())
-
-random.seed(0)
-print(random.random())
-print(random.random())
-print(random.random())
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
-
-print(random.choice(numbers))
-print(random.choice(names))
-
-
-random.choice('CanYouFeelThePowerOfPython')
+# random.seed(random.random())
+# print(random.random())
+# print(random.random())
+# print(random.random())
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
 
-for i in range(10):
-    print(random.choice(numbers))
+# print(random.choice(numbers))
+# print(random.choice(names))
+# print(random.randint(1, 5))
+
+
+# print(random.choice('CanYouFeelThePowerOfPython'))
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for i in range(10):
+#     print(random.choice(numbers))
+
+# names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
+
+# print(random.sample(names, 6))
+
+
+# names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
+
+# random.sample(names, 7)
 
 names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
 
-random.sample(names, 6)
-
-
-names = ['Alan', 'Kate', 'Mary', 'Kate', 'Jo', 'John']
-
-random.sample(names, 7)
+print(random.sample(names, 3))
