fd46b7a700481e072ec44c5a92e32e8c1faa418e
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-07 07:52:05 -0500

    updates

--- 07_Object Oriented Programming/04_Class and Instance Variables/notes.py
+++ 07_Object Oriented Programming/04_Class and Instance Variables/notes.py
@@ -1,80 +1,73 @@
-class Dog():
-    counter = 0
-
-    def __init__(self, name, age):
-        self.__name = name
-        self.age = age
-        Dog.counter += 1
+# class Dog():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
 
-my_pet = Dog('Teddy', 2)
-kates_pet = Dog('Foxy', 5)
-adams_pet = Dog('Luna', 1)
+# class Dog():
+#     counter = 0
 
-print(my_pet.counter)
-print(kates_pet.counter)
-print(adams_pet.counter)
-
-
-class Dog():
-    __counter = 0
-
-    def __init__(self, name, age):
-        self.__name = name
-        self.age = age
-        Dog.__counter += 1
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.age = age
+#         Dog.counter += 1
 
 
-class Dog():
-    counter = 0
+# my_pet = Dog('Teddy', 2)
+# kates_pet = Dog('Foxy', 5)
+# adams_pet = Dog('Luna', 1)
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        Dog.counter += 1
+# print(my_pet.counter)
+# print(kates_pet.counter)
+# print(adams_pet.counter)
 
-
-my_pet = Dog('Tom', 5)
-
-if hasattr(my_pet, 'name'):
-    print('My pet is called', my_pet.name)
-else:
-    print('My pet has no name')
-
-if hasattr(Dog, 'counter'):
-    print('The Dog class has a counter!')
-else:
-    print('The Dog class does not have a counter')
+# print(Dog.counter)
+# Dog.counter += 1
+# print(Dog.counter)
 
 
-class House():
-    counter = 0
+# class Dog():
+#     __counter = 0
 
-    def __init__(self, address, area, price):
-        self.address = address
-        self.area = area
-        self.price = price
-        House.counter += 1
+#     def get_count():
+#         return Dog.__counter
 
-        House.quality = 'low'
-        self.quality = 'medium'
-        quality = 'high'
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.age = age
+#         Dog.__counter += 1
 
-        print(House.quality, self.quality, quality)
+# my_pet = Dog('Teddy', 2)
+# kates_pet = Dog('Foxy', 5)
+# adams_pet = Dog('Luna', 1)
 
-    def present(self):
-        print('The house at', self.address, 'has an area of',
-              self.area, 'and costs', self.price)
+# print(Dog.get_count())
 
 
-soho_house = House('12 Lexington St, Soho', 130, 540000)
-soho_house.present()
-print(House.counter)
+class MathUtility:
+    __pi = 3.14159265358979323646233
+    __e = 2.718281828459045
+    
+    def pi():
+        return MathUtility.__pi 
+    
+    def e():
+        return MathUtility.__e
+    
+    def abs(number):
+        if(number < 0):
+            return -number
+        else:
+            return number
+        
+print(MathUtility.pi())
+print(MathUtility.e())
+print(MathUtility.abs(-5))
+            
+
+    
 
 
-class Dog():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+
 
 
