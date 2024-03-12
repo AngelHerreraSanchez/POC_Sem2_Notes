151474b05bbdf0936653e95bf4a254ef23b532a7
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-12 07:57:07 -0400

    updates

--- 07_Object Oriented Programming/07_Inheritance/notes.py
+++ 07_Object Oriented Programming/07_Inheritance/notes.py
@@ -1,139 +1,78 @@
-class Vehicle:
-    pass
+# class Vehicle:
+#     pass
 
 
-class LandVehicle(Vehicle):
-    pass
+# class LandVehicle(Vehicle):
+#     pass
 
 
-class Car(LandVehicle):
-    pass
+# class Car(LandVehicle):
+#     pass
 
 
-issubclass(Car, LandVehicle)
+# print(issubclass(Car, LandVehicle))
 
-issubclass(LandVehicle, Vehicle)
+# print(issubclass(LandVehicle, Vehicle))
 
-issubclass(Car, Vehicle)
-issubclass(Car, Car)
-
-
-class Vehicle:
-    class_message = 'This is a message from the Vehicle class!'
-
-    def __init__(self, speed):
-        self.speed = speed
+# print(issubclass(Car, Vehicle))
+# print(issubclass(Car, Car))
 
 
-class LandVehicle(Vehicle):
-    def __init__(self, speed, wheel_count):
-        super().__init__(speed)
-        self.wheel_count = wheel_count
-        print(super().class_message)
+# class Vehicle:
+#     class_message = 'This is a message from the Vehicle class!'
+
+#     def __init__(self, speed):
+#         self.speed = speed
 
 
-class Car(LandVehicle):
-    pass
+# class LandVehicle(Vehicle):
+#     def __init__(self, speed, wheel_count):
+#         super().__init__(speed)
+#         self.wheel_count = wheel_count
+#         print(super().class_message)
 
 
-my_car = Car(50, 4)
-
-my_car.class_message
-
-
-class Vehicle:
-    class_message = 'This is a message from the Vehicle class!'
-
-    def __init__(self, speed):
-        self.speed = speed
+# class Car(LandVehicle):
+#     pass
 
 
-class LandVehicle(Vehicle):
-    def __init__(self, speed, wheel_count):
-        super().__init__(speed)
-        self.wheel_count = wheel_count
-        print(super().class_message)
+# my_car = Car(50, 4)
 
-    def speed_up(self):
-        self.speed += 5
+# print(my_car.class_message)
+# print(my_car.speed)
+# print(my_car.wheel_count)
 
 
-class Car(LandVehicle):
-    def super_speed(self):
-        print('Super speed activated!')
-        super().speed_up()
-        super().speed_up()
-        super().speed_up()
+# class Vehicle:
+#     class_message = 'This is a message from the Vehicle class!'
+
+#     def __init__(self, speed):
+#         self.speed = speed
+
+
+# class LandVehicle(Vehicle):
+#     def __init__(self, speed, wheel_count):
+#         super().__init__(speed)
+#         self.wheel_count = wheel_count
+#         print(super().class_message)
+
+#     def speed_up(self):
+#         self.speed += 5
+
+
+# class Car(LandVehicle):
+#     def super_speed(self):
+#         print('Super speed activated!')
+#         super().speed_up()
+#         super().speed_up()
+#         super().speed_up()
         
 
-my_fast_car = Car(10, 4)
-print(my_fast_car.__dict__)
-my_fast_car.super_speed()
-print(my_fast_car.__dict__)
-my_fast_car.speed_up()
-print(my_fast_car.__dict__)
-
-
-class Vehicle:
-    pass
-
-
-class LandVehicle(Vehicle):
-    pass
-
-
-class Car(LandVehicle):
-    pass
-
-
-my_car = Car()
-print(my_car.__dict__)
-
-class Vehicle:
-    def __init__(self, speed):
-        self.speed = speed
-
-class LandVehicle(Vehicle):
-    pass
-
-class Car(LandVehicle):
-    pass
-
-my_car = Car(5)
-print(my_car.__dict__)
-
-
-class Vehicle:
-    def __init__(self, speed):
-        self.speed = speed
-
-
-class LandVehicle(Vehicle):
-    def __init__(self, wheel_count):
-        self.wheel_count = wheel_count
-
-
-class Car(LandVehicle):
-    pass
-
-my_car = Car(5)
-print(my_car.__dict__)
-
-class Vehicle:
-    def __init__(self, speed):
-        self.speed = speed
-
-class LandVehicle(Vehicle):
-    def __init__(self, speed, wheel_count):
-        super().__init__(speed)
-        self.wheel_count = wheel_count
-
-class Car(LandVehicle):
-    pass
-
-my_car = Car(5, 10)
-print(my_car.__dict__)
-
-my_car.speed
+# my_fast_car = Car(10, 4)
+# print(my_fast_car.__dict__)
+# my_fast_car.super_speed()
+# print(my_fast_car.__dict__)
+# my_fast_car.speed_up()
+# print(my_fast_car.__dict__)
 
 
