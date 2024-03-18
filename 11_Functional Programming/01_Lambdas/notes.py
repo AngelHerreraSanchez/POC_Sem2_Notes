26b05121fd93d26b80ba59a305040dfc7eb890b7
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-18 11:49:47 -0400

    updates

--- 11_Functional Programming/01_Lambdas/notes.py
+++ 11_Functional Programming/01_Lambdas/notes.py
@@ -1,8 +1,8 @@
-def function_name(parameter1, parameter2):
-  #instruction1
-  #instruction2
+# def function_name(parameter1, parameter2):
+#   #instruction1
+#   #instruction2
   
-lambda parameter1, parameter2: #instruction
+# lambda parameter1, parameter2: #instruction
     
 def sum(a, b):
     return a + b
@@ -11,24 +11,26 @@ lambda a, b: a + b
 
 lambda x, y: x + y
 
-sum(5, 3)
+# print(sum(5, 3))
 
-another_sum = lambda a, b: a + b
+# another_sum = lambda a, b: a + b
 
-another_sum(5, 3)
+# print(another_sum(5, 3))
 
-def apply_func(elements, func):
-    for element in elements:
-        print(func(element))
+# def apply_func(elements, func):
+#     for element in elements:
+#         print(func(element))
         
-my_func = lambda x: x * x
-apply_func([1, 2, 3, 4, 5], my_func)
+# my_func = lambda x: x * x
+# apply_func([1, 2, 3, 4, 5], my_func)
 
-my_func = lambda x: 1/x
-apply_func([1, 2, 3, 4, 5], my_func)
+# my_func = lambda x: 1/x
+# apply_func([1, 2, 3, 4, 5], my_func)
 
-my_func = lambda x: 0
-apply_func([1, 2, 3, 4, 5], my_func)
+# my_func = lambda x: 0
+# apply_func([1, 2, 3, 4, 5], my_func)
 
-apply_func([1, 2, 3, 4, 5], lambda x: x * x)
+# apply_func([1, 2, 3, 4, 5], lambda x: x * x)
+
+# my_func2 = lambda x: 1 if x else 2
 
