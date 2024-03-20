6b37cdec5f84d4b21eff7fea4189a08fdae78dff
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-20 07:47:33 -0400

    updates

--- 11_Functional Programming/02_Maps, Filter, Closures/notes.py
+++ 11_Functional Programming/02_Maps, Filter, Closures/notes.py
@@ -1,60 +1,59 @@
-def lambda_func(i): return i * 2
+# def lambda_func(i): return i * 2
+# my_func = lambda i: i * 2
+
+# initial_list = [1, 2, 3, 4, 5]
 
 
-initial_list = [1, 2, 3, 4, 5]
 
+# map_result = map(lambda_func, initial_list)
+# print(map_result)
+# print(next(map_result))
+# print(next(map_result))
+# print(next(map_result))
+# print(next(map_result))
+# print(next(map_result))
+# print(next(map_result))
 
-map(lambda_func, initial_list)
-
-
-map_result = map(lambda_func, initial_list)
-print(next(map_result))
-print(next(map_result))
-print(next(map_result))
-print(next(map_result))
-print(next(map_result))
-print(next(map_result))
-
-map_result = map(lambda_func, initial_list)
-for element in map_result:
-    print(element, end=',')
+# map_result = map(lambda_func, initial_list)
+# for element in map_result:
+#     print(element, end=',')
     
-    map_result = map(lambda_func, initial_list)
-print(list(map_result))
+# map_result = map(lambda_func, initial_list)
+# new_list = list(map_result)
+# print(new_list)
+
+# map_result = map(my_func, initial_list)
+# new_list = list(map_result)
+# print(new_list)
+
+# # def lambda_func(i): return i * 2
 
 
-def lambda_func(i): return i * 2
+# print(list(map(lambda i: i * 2, [1, 2, 3, 4, 5])))
+
+# print(list(filter(lambda i: i % 2 == 0, [1, 2, 3, 4, 5, 6, 7, 8])))
+
+# emails = [
+#     'frank@gmail.com',
+#     'i love python',
+#     '98237434',
+#     'jonsmith@yahoo.com',
+#     'whereareyou@mywebsite.co.uk',
+#     'fs3dfss'
+# ]
+# emails = list(filter(lambda x: '@' in x, emails))
+# print(emails)
+
+# def greet(text):
+
+#     def print_greet():
+#         print(text)
+
+#     return print_greet
 
 
-initial_list = [1, 2, 3, 4, 5]
-map_result = map(lambda_func, initial_list)
-print(list(map_result))
-
-print(list(map(lambda i: i * 2, [1, 2, 3, 4, 5])))
-
-print(list(filter(lambda i: i % 2 == 0, [1, 2, 3, 4, 5, 6, 7, 8])))
-
-emails = [
-    'frank@gmail.com',
-    'i love python',
-    '98237434',
-    'jonsmith@yahoo.com',
-    'whereareyou@mywebsite.co.uk',
-    'fs3dfss'
-]
-list(filter(lambda x: '@' in x, emails))
-
-
-def greet(text):
-
-    def print_greet():
-        print(text)
-
-    return print_greet
-
-
-say_hello = greet('Hello')
-say_hello()
+# say_hello = greet('Hello')
+# say_hello()
 
 
 def make_multiply_closure(x):
