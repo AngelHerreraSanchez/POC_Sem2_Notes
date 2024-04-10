95555bba48b302dbcdc1ee4431e4f13394632689
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-04-10 07:33:58 -0400

    updates

--- 12_GameDesign_1/tic_tac_toe.py
+++ 12_GameDesign_1/tic_tac_toe.py
@@ -4,8 +4,6 @@ grid = [
     ["7", "8", "9"],
 ]
 
-current_player = "X"
-
 def print_grid():
     for row in range(len(grid)):
         for col in range(len(grid[row])):
@@ -14,38 +12,14 @@ def print_grid():
             else:
                 print(grid[row][col])
                 
-def handle_input(value):
-    if(value.__eq__("STOP")):
-        return False
-    elif(value.__eq__("1")):
-        return False
-    elif (value.__eq__("2")):
-        return False
-    elif (value.__eq__("3")):
-        return False
-    elif(value.__eq__("4")):
-        return False
-    elif (value.__eq__("5")):
-        return False
-    elif(value.__eq__("6")):
-        return False
-    elif(value.__eq__("7")):
-        return False
-    elif(value.__eq__("8")):
-        return False
-    elif (value.__eq__("9")):
-        return False
-    else:
-        return True
-        
+       
 
 def game_loop():
     print("Welcome to TIC TAC TOE")
     user_choice = ""
     while(True):
         print_grid()
-        while (handle_input(user_choice)):
-            user_choice = input("Enter STOP to end.  Or a number (1-9) where to put the piece: ")
+        user_choice = input("Enter STOP to end.  Or a number (1-9) where to put the piece: ")
         if user_choice.__eq__("STOP"):
             break
         print("Game Loop Running")
